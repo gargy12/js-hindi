f896dab001a7c05c91c5d6f9b4521db5e81532c0
gargy12 <168583976+gargy12@users.noreply.github.com>
2024-08-28 17:36:09 +0000

    Operations

--- 01_Basics/03_conversionOperator.js
+++ 01_Basics/03_conversionOperator.js
@@ -1,12 +1,12 @@
 let score = 34
 
-console.log(typeof "score");
-console.log(typeof (score));
+// console.log(typeof "score");
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
 
-console.log(typeof (valueInNumber));
-console.log(valueInNumber);
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
 
 //"33" => 33
 //"33a"=> NaN (Not a Number)
@@ -14,11 +14,36 @@ console.log(valueInNumber);
 
 let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 let s = 10
 
-letstringNumber = String(s)
-console.log(typeof stringNumber);
-console.log(s);
+// letstringNumber = String(s)
+
+
+
+// ************************** Operations *********************
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "Hi"
+let str2 = " HW"
+let str3 = str1+str2
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2); //if string is at first than the values will be treated as string
+// console.log(1 + 2 + "2"); //if string is at last than first conversion takes places  
+
+// console.log((3 + 4) * 5  % 3);
+
+console.log(+true);
+
+
+
+
