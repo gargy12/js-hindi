4277a4e23efe16f775219bfd4cdddcc70dfaa958
gargy12 <168583976+gargy12@users.noreply.github.com>
2024-08-28 11:53:09 +0000

    #

--- 01_Basics/02_datatypes.js
+++ 01_Basics/02_datatypes.js
@@ -2,11 +2,24 @@
 
 // alert (3+3) // we are using node js not browser
 
-console.log(3
+// console.log(3
     +3
-)// Readibility should be high
+// Readibility should be high
 
 
-let name = "Yash" //String Data type//
-let age = 18 //Num Data type //
-let isloggedIn = True // Boolean //
+ let name = "Yash" //String Data type//
+// let age = 18 //Num Data type //
+ //let isloggedIn = True // Boolean //
+
+//number = 2 
+//bigint
+//string =""
+//boolean = true/false
+//null => Stand alone value
+//undefined = 
+//symbol = uniqueness 
+
+console.log(typeof "Yash"); // typeof tells datatype
+console.log(typeof null); // output object
+console.log(typeof undefined); // output undefined
+
