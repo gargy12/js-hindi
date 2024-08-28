deb1176a99042bd7d3f5982495da3f6148b87384
gargy12 <168583976+gargy12@users.noreply.github.com>
2024-08-28 15:34:22 +0000

    Conversion

---
+++ 01_Basics/03_conversionOperator.js
@@ -0,0 +1,18 @@
+let score = 34
+
+console.log(typeof "score");
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof (valueInNumber));
+console.log(valueInNumber);
+
+//"33" => 33
+//"33a"=> NaN (Not a Number)
+// true =>1; false=>0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
