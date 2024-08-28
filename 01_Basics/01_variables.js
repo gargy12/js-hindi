cbbd4fec514df20939200842eb3a4e6bc4a73700
gargy12 <168583976+gargy12@users.noreply.github.com>
2024-08-28 09:51:09 +0000

    Varraible

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1
+let accountEmail = "yash123@gmail.com"
+/*var accountPassword = "123"*/
+accountCity = "Noida"
+
+accountEmail ="a12@gmail.com"
+accountPassword = "11"
+accountCity = "aag"
+
+console.log(accountEmail);
+
+console.table([accountId , accountEmail , accountPassword , accountCity])
