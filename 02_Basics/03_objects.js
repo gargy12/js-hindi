f86e03890d0412faadcccda45c594952ff4dac20
gargy12 <168583976+gargy12@users.noreply.github.com>
2024-08-30 14:26:25 +0000

    objects part 1

---
+++ 02_Basics/03_objects.js
@@ -0,0 +1,39 @@
+// objects can be declared in two ways literals and constructor
+
+// singleton when an object is created using constructor only singleton object is created
+
+// when an object is created using literals than object is not created as singleton
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
