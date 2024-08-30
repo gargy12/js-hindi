6892a6da9bb46c0277b0f55f8e9a75dbf7374457
gargy12 <168583976+gargy12@users.noreply.github.com>
2024-08-30 04:58:20 +0000

    Strings

---
+++ 01_Basics/05_string.js
@@ -0,0 +1,20 @@
+const name = "yash"
+const repoCount = 10
+
+//console.log(name + repoCount + "value");
+
+// backticks use string interpolation in this placeholders are created and the value is directly injected into them
+
+// console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`); // $ is used to enter values
+
+// new is used to declare string
+
+const gameName = new String ('Yash')
+
+
+console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
