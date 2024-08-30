8dd98e65ff9c5ef26c95049e6b356c34b6b105f8
gargy12 <168583976+gargy12@users.noreply.github.com>
2024-08-30 10:57:09 +0000

    Array updated

--- 02_Basics/01_array.js
+++ 02_Basics/01_array.js
@@ -4,3 +4,9 @@
 
 const myArr = [0,1,2,3,4]
 console.log(myArr);
+console.log(myArr["1"]);
+
+/* shalllow copy of an object is a copy whose 
+ properties share the samre references as that of objects */
+
+ /* Deep copy of an object do not share the same refernce point
