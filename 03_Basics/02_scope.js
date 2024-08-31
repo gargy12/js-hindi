b18ff9b04b797b40cb22df6f3ccf4b013c864592
gargy12 <168583976+gargy12@users.noreply.github.com>
2024-08-31 19:06:28 +0000

    Scope

---
+++ 03_Basics/02_scope.js
@@ -0,0 +1,14 @@
+// scope keywords let var and const
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+} // block scope and gloggal scope written out of block
+
+
+//{} is known as scope
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
