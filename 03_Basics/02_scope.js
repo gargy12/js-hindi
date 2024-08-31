dcc5838c77bb7f1ef7123b0fb560519edc8abbb8
gargy12 <168583976+gargy12@users.noreply.github.com>
2024-08-31 20:26:00 +0000

    scope

--- 03_Basics/02_scope.js
+++ 03_Basics/02_scope.js
@@ -3,7 +3,7 @@ let a = 300
 if (true) {
     let a = 10
     const b = 20
-     console.log("INNER: ", a);
+   //  console.log("INNER: ", a);
 } // block scope and gloggal scope written out of block
 
 //{} is known as scope
@@ -19,10 +19,38 @@ function one(){
 
     function two(){
         const website = "youtube"
-        console.log(username);
+     //   console.log(username);
     } // known as closure
    // console.log(website);
     two()
 }
 
 one()
+
+if (true) {
+    const username = "Yash"
+    if (username === "Yash") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+// ++++++++++++++interesting +++++++++++
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+//addTwo(5)
+// const addTwo = function(num){ // also known as expression
+//     return num + 2
+    
+// }
+
