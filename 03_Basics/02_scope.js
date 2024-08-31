f0179366cb15829649f306ad0b355272dff8c421
gargy12 <168583976+gargy12@users.noreply.github.com>
2024-08-31 19:22:21 +0000

    scopes

--- 03_Basics/02_scope.js
+++ 03_Basics/02_scope.js
@@ -3,12 +3,26 @@ let a = 300
 if (true) {
     let a = 10
     const b = 20
-    // console.log("INNER: ", a);
+     console.log("INNER: ", a);
 } // block scope and gloggal scope written out of block
-
 
 //{} is known as scope
 
 // console.log(a);
 // console.log(b);
 // console.log(c);
+
+//nested scope
+ 
+function one(){
+    const username = "yash"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    } // known as closure
+   // console.log(website);
+    two()
+}
+
+one()
