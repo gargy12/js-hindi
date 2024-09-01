2c054c2623d949b2a368d244fd849789dc5a8094
gargy12 <168583976+gargy12@users.noreply.github.com>
2024-09-01 08:59:28 +0000

    switch case

---
+++ 04_control_flow /switch.js
@@ -0,0 +1,33 @@
+//switch case
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// } Basic syntax
+
+// ctrl + shift + down arrow key used to create duplicate
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
